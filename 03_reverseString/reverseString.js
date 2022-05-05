786f4268a99a48a8f47d700567583fb580dbc0b6
Osama Shbib <osamashbib28@gmail.com>
2022-05-05 19:23:55 +0300

    Finish Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const n = str.length;
+    let finalString  = "";
+    if (str === "") {
+        return "";
+    }
+    for (let i = n - 1; i >= 0; i--) {
+        finalString += str[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
