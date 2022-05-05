28017391ec42ac464faad645c80c3fcf9fbd22fa
Osama Shbib <osamashbib28@gmail.com>
2022-05-05 19:57:35 +0300

    Finish Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let result = 0;
+    if (typeof a != 'number' | typeof b != 'number') {
+        return "ERROR";
+    }
+    if (a < 0 | b < 0) {
+        return "ERROR";
+    }
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+    for (let i = a; i <= b; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
