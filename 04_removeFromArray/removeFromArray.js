5485981cd56c9b34601d99befc50694a22a21ab5
Osama Shbib <osamashbib28@gmail.com>
2022-05-05 19:50:26 +0300

    Finish Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arg) {
+    let indexes = [];
+    for (let i = 0; i < arg.length; i++) {
+        if (arr.indexOf(arg[i]) != -1) {
+            indexes.push(arr.indexOf(arg[i]));
+        }
+    }
+    indexes.sort();
+    for (let i = indexes.length - 1; i >= 0; i--) {
+        arr.splice(indexes[i], 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
