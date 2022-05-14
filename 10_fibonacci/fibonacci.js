2e4e903d705de4c5cfb473478286a68ebdd24412
Osama Shbib <osamashbib28@gmail.com>
2022-05-14 19:54:08 +0300

    Finish Fibonacci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fibList = [1,1]
+    while (num > fibList.length) {
+        fibList.push(fibList[fibList.length - 1] + fibList[fibList.length - 2]);
+    }
+    return fibList[num - 1];
 };
 
 // Do not edit below this line
