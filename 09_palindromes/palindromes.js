c8d4b5c6cbdfd35daf8000804216c709dc669311
Osama Shbib <osamashbib28@gmail.com>
2022-05-14 19:42:16 +0300

    Finish Palindrome Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+function isLetter(c) {
+    if (c == " ") {
+        return false;
+    }
+    return c.toLowerCase() != c.toUpperCase();
+  }
 
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let clean = "";
+    for (let i = 0; i < str.length; i++) {
+        const letter = str[i];
+        if (isLetter(letter)) {
+            clean += letter;
+        }
+    }
+    let isPalindrome = true;
+    for (let i = 0; i < Math.floor(clean.length / 2); i++) {
+        const letter = clean[i];
+        if (letter != clean[clean.length - 1 - i]) {
+            isPalindrome = false;
+        }
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
